dd6fa733947f36c18df21433a62f044759aed7e8
Enoch7768 <e.elijah2023@gmail.com>
2026-09-10 02:07:26 +0300

    Allow web chat to select provider and model

--- internal/app/web.go
+++ internal/app/web.go
@@ -2,6 +2,18 @@ package app
 
 import "context"
 
-func (a *App) ChatRequest(ctx context.Context, prompt string) error {
+func (a *App) ChatRequest(ctx context.Context, prompt, providerName, model string) error {
+	if providerName != "" {
+		a.Config.DefaultProvider = providerName
+	}
+
+	if providerName != "" && model != "" {
+		pc, ok := a.Config.Providers[providerName]
+		if ok {
+			pc.DefaultModel = model
+			a.Config.Providers[providerName] = pc
+		}
+	}
+
 	return a.chatTurn(ctx, prompt)
 }
