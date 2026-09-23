2ff119ced10ab4f50a39373bc41b9fdf5850639e
Enoch7768 <e.elijah2023@gmail.com>
2026-09-23 15:11:49 +0300

    Add catalog provider package required by CI

---
+++ internal/provider/scaleway/scaleway.go
@@ -0,0 +1,4 @@
+package scaleway
+
+import("github.com/Enoch7768/fuzecli/internal/provider";"github.com/Enoch7768/fuzecli/internal/provider/catalogruntime")
+func New(apiKey,baseURL string) provider.Provider{return catalogruntime.New("scaleway",apiKey,baseURL,"SCALEWAY_API_KEY",22000,3500,3000)}
