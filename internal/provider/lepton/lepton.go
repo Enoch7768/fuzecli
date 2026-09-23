ff3f33da04817037952a637b9017335a9ec3e3d7
Enoch7768 <e.elijah2023@gmail.com>
2026-09-23 15:11:57 +0300

    Add catalog provider package required by CI

---
+++ internal/provider/lepton/lepton.go
@@ -0,0 +1,4 @@
+package lepton
+
+import("github.com/Enoch7768/fuzecli/internal/provider";"github.com/Enoch7768/fuzecli/internal/provider/catalogruntime")
+func New(apiKey,baseURL string) provider.Provider{return catalogruntime.New("lepton",apiKey,baseURL,"LEPTON_API_TOKEN",18000,2500,2200)}
