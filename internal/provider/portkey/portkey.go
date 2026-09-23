6fdfe2cc7f3ed2c0b8274f5a2ff07550a8216513
Enoch7768 <e.elijah2023@gmail.com>
2026-09-23 15:12:04 +0300

    Add catalog provider package required by CI

---
+++ internal/provider/portkey/portkey.go
@@ -0,0 +1,4 @@
+package portkey
+
+import("github.com/Enoch7768/fuzecli/internal/provider";"github.com/Enoch7768/fuzecli/internal/provider/catalogruntime")
+func New(apiKey,baseURL string) provider.Provider{return catalogruntime.New("portkey",apiKey,baseURL,"PORTKEY_API_KEY",22000,3500,3000)}
