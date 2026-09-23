8722023231c618aeff863272e5df5b16008d53f1
Enoch7768 <e.elijah2023@gmail.com>
2026-09-23 15:11:39 +0300

    Add catalog provider package required by CI

---
+++ internal/provider/lambda/lambda.go
@@ -0,0 +1,4 @@
+package lambda
+
+import("github.com/Enoch7768/fuzecli/internal/provider";"github.com/Enoch7768/fuzecli/internal/provider/catalogruntime")
+func New(apiKey,baseURL string) provider.Provider{return catalogruntime.New("lambda",apiKey,baseURL,"LAMBDA_API_KEY",22000,3500,3000)}
