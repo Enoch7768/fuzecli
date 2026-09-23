cd2205f48ca5c82d40f4ff5cd013c92db6bdfc93
Enoch7768 <e.elijah2023@gmail.com>
2026-09-23 15:12:44 +0300

    Add catalog provider package required by CI

---
+++ internal/provider/jan/jan.go
@@ -0,0 +1,4 @@
+package jan
+
+import("github.com/Enoch7768/fuzecli/internal/provider";"github.com/Enoch7768/fuzecli/internal/provider/catalogruntime")
+func New(apiKey,baseURL string) provider.Provider{return catalogruntime.New("jan",apiKey,baseURL,"JAN_API_KEY",10000,2200,1800)}
