b31ca63a2c47667269d4445048c42f635217acae
Enoch7768 <e.elijah2023@gmail.com>
2026-09-25 22:48:54 +0300

    feat: serialize provider capabilities for web model discovery

--- internal/provider/capabilities.go
+++ internal/provider/capabilities.go
@@ -1,14 +1,14 @@
 package provider
 
 type Capabilities struct {
-	Streaming        bool
-	StructuredJSON   bool
-	ListModels       bool
-	ToolCalling      bool
-	Vision           bool
-	MaxInputChars    int
-	MaxOutputTokens  int
-	JSONOutputTokens int
+	Streaming        bool `json:"streaming"`
+	StructuredJSON   bool `json:"structured_json"`
+	ListModels       bool `json:"list_models"`
+	ToolCalling      bool `json:"tool_calling"`
+	Vision           bool `json:"vision"`
+	MaxInputChars    int `json:"max_input_chars"`
+	MaxOutputTokens  int `json:"max_output_tokens"`
+	JSONOutputTokens int `json:"json_output_tokens"`
 }
 
 type CapabilityProvider interface {
